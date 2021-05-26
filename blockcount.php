24284af6e94bee835e15831733c65ef08deab185
Nicolas Renault <nrenault@noriade.com>
2021-05-27 05:27:27 +0700

    Update blockcount.php

--- blockcount.php
+++ blockcount.php
@@ -15,6 +15,14 @@ $getblockcount = json_decode($getblockcount);
 $getblockcount = $getblockcount->{'result'};
 curl_close($curl);
 
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://explorer.nyxcoin.org/api/getblockcount"
+));
+$getreportedblock = curl_exec($curl);
+$getreportedblock = json_decode($getreportedblock);
+$getreportedblock = $getreportedblock->{'result'};
+
 if(!empty($getblockcount)){
 
 // if((int)$getblockcount < (int)$getreportedblock){
