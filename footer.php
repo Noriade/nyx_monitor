62d00ddb2260fc8ff072d2e0484db0f029ae7057
Nicolas Renault <nrenault@noriade.com>
2021-05-27 12:03:12 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,12 +2,12 @@
     <h3 class="w3-bottombar w3-border-blue">Support NYX</h3>
     <p>Source code on <a href="https://github.com/nrenault/nyx_monitor" target="_blank">GitHub</a> supported by <a href="https://noriade.com" target="_blank">Noriade</a></p>
     <p>NYX Links :
-      <a href="http://explorer.nyxcoin.org/" target="_blank">Explorer</a> |
-      <a href="https://www.nyxcoin.org/" target="_blank">Website</a> |   
-      <a href="https://twitter.com/NYXCOIN" target="_blank">Twitter</a> |
-      <a href="https://web.facebook.com/NYX-Coin-306423023208595/" target="_blank">Facebook</a> |
-      <a href="https://github.com/nyxpay/nyx" target="_blank">Github</a> |
-      <a href="https://discord.gg/AJhBZ7R" target="_blank">Discord</a> |
-      <a href="https://join.slack.com/t/nyxcoin/shared_invite/enQtMzA1OTU5NjA1MTcyLTlhOTMxZTcxMTBhMTA5MWZhNjIxMTMxOGMwODVkN2ZiNWVlOTFhZWVkZDU2NDUzZjllOTdjYzU5YzFlM2U2MmY" target="_blank">Slack</a>
+      <a href="http://explorer.nyxcoin.org/" target="_blank"><i class="fas fa-search fa-2x"></i></a> |
+      <a href="https://www.nyxcoin.org/" target="_blank"><i class="fas fa-external-link fa-2x"></i></a> |   
+      <a href="https://twitter.com/NYXCOIN" target="_blank"><i class="fab fa-twitter fa-2x"></i></a> |
+      <a href="https://web.facebook.com/NYX-Coin-306423023208595/" target="_blank"><i class="fab fa-facebook fa-2x"></i></a> |
+      <a href="https://github.com/nyxpay/nyx" target="_blank"><i class="fab fa-github fa-2x"></i></a> |
+      <a href="https://discord.gg/AJhBZ7R" target="_blank"><i class="fab fa-discord fa-2x"></i></a> |
+      <a href="https://join.slack.com/t/nyxcoin/shared_invite/enQtMzA1OTU5NjA1MTcyLTlhOTMxZTcxMTBhMTA5MWZhNjIxMTMxOGMwODVkN2ZiNWVlOTFhZWVkZDU2NDUzZjllOTdjYzU5YzFlM2U2MmY" target="_blank"><i class="fab fa-slack fa-2x"></i></a>
     </p>
   </footer>
