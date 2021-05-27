b550fe5c198a1cb29fd55b8d5aaf061a23cd5b96
Nicolas Renault <nrenault@noriade.com>
2021-05-27 11:36:04 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+  <footer class="w3-container w3-padding-16 w3-dark-grey">
+    <h3 class="w3-bottombar w3-border-blue">Support NYX</h3>
+    <p>Source code on <a href="https://github.com/nrenault/nyx_monitor" target="_blank">GitHub</a></p>
+    <p>NYX Links :
+      <a href="http://explorer.nyxcoin.org/" target="_blank">Explorer</a> |
+      <a href="https://discord.gg/AJhBZ7R" target="_blank">Discord</a> |
+      <a href="https://www.nyxcoin.org/" target="_blank">Website</a> |
+      <a href="https://join.slack.com/t/nyxcoin/shared_invite/enQtMzA1OTU5NjA1MTcyLTlhOTMxZTcxMTBhMTA5MWZhNjIxMTMxOGMwODVkN2ZiNWVlOTFhZWVkZDU2NDUzZjllOTdjYzU5YzFlM2U2MmY" target="_blank">Slack</a>
+    </p>
+  </footer>
