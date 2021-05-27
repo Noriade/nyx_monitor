9b516f0e0dfa4819e2a65683535cc09cd5435907
Nicolas Renault <nrenault@noriade.com>
2021-05-27 11:53:49 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,13 @@
   <footer class="w3-container w3-padding-16 w3-dark-grey">
     <h3 class="w3-bottombar w3-border-blue">Support NYX</h3>
-    <p>Source code on <a href="https://github.com/nrenault/nyx_monitor" target="_blank">GitHub</a></p>
+    <p>Source code on <a href="https://github.com/nrenault/nyx_monitor" target="_blank">GitHub</a> supported by <a href="https://noriade.com" target="_blank">Noriade</a></p>
     <p>NYX Links :
       <a href="http://explorer.nyxcoin.org/" target="_blank">Explorer</a> |
+      <a href="https://www.nyxcoin.org/" target="_blank">Website</a> |   
+      <a href="https://twitter.com/NYXCOIN" target="_blank">Twitter</a> |
+      <a href="https://web.facebook.com/NYX-Coin-306423023208595/" target="_blank">Facebook</a> |
+      <a href="https://github.com/nyxpay/nyx" target="_blank">Github</a> |
       <a href="https://discord.gg/AJhBZ7R" target="_blank">Discord</a> |
-      <a href="https://www.nyxcoin.org/" target="_blank">Website</a> |
       <a href="https://join.slack.com/t/nyxcoin/shared_invite/enQtMzA1OTU5NjA1MTcyLTlhOTMxZTcxMTBhMTA5MWZhNjIxMTMxOGMwODVkN2ZiNWVlOTFhZWVkZDU2NDUzZjllOTdjYzU5YzFlM2U2MmY" target="_blank">Slack</a>
     </p>
   </footer>
