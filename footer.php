247a5f8d4f0529c108dd2c4192ed575f472440ec
Nicolas Renault <nrenault@noriade.com>
2021-05-27 12:04:25 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
     <p>Source code on <a href="https://github.com/nrenault/nyx_monitor" target="_blank">GitHub</a> supported by <a href="https://noriade.com" target="_blank">Noriade</a></p>
     <p>NYX Links :
       <a href="http://explorer.nyxcoin.org/" target="_blank"><i class="fas fa-search fa-2x"></i></a> |
-      <a href="https://www.nyxcoin.org/" target="_blank"><i class="fas fa-external-link fa-2x"></i></a> |   
+      <a href="https://www.nyxcoin.org/" target="_blank"><i class="fas fa-link fa-2x"></i></a> |   
       <a href="https://twitter.com/NYXCOIN" target="_blank"><i class="fab fa-twitter fa-2x"></i></a> |
       <a href="https://web.facebook.com/NYX-Coin-306423023208595/" target="_blank"><i class="fab fa-facebook fa-2x"></i></a> |
       <a href="https://github.com/nyxpay/nyx" target="_blank"><i class="fab fa-github fa-2x"></i></a> |
